0f070e0b5370ceaf56930a81d32791eb7260dd01
Masataro Asai <guicho2.71828@gmail.com>
2016-06-27 11:20:02 +0900

    plot-all in parallel

--- plot-all.sh
+++ plot-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./plot-allmacro-eval.lisp
-./plot-allmacro-gen.lisp
-./plot-allmacro-exp.lisp
+./plot-allmacro-eval.lisp &
+./plot-allmacro-gen.lisp &
+./plot-allmacro-exp.lisp &
+./plot-mangle-eval.lisp &
+./plot-mangle-gen.lisp &
+./plot-mangle-exp.lisp &
+wait
