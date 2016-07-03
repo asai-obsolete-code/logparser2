63ded58e913a561421d772d1efd73874ddb50f8b
Masataro Asai <guicho2.71828@gmail.com>
2016-07-03 17:13:19 +0900

    use ramdisk

---
+++ store-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+copytoram (){
+    rsync -rL --exclude="*.qsub" --exclude="*.pddl" --exclude="*.1" $1 /run/shm/
+}
+
+main (){
+    trap "rm -r /run/shm/$1" RETURN SIGINT SIGTERM
+    copytoram $1
+    find -L /run/shm/$1 -name "*.out" | xargs ./store.bin
+}
+
+
+for d in */
+do
+    main $d
+done
