70cfcc9a08167b656843a4e655758c921dfae446
Masataro Asai <guicho2.71828@gmail.com>
2016-07-07 10:31:49 +0900

    fig3

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@
 set -o errexit
 
 [ $(sqlite3 db.sqlite "select count(*) from fig2") -eq $(find -L fig2*/ -name "*.out" | wc -l) ]
+[ $(sqlite3 db.sqlite "select count(*) from fig3") -eq $(find -L fig3*/ -name "*.out" | wc -l) ]
